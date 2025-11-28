99a0b728417be96fdbcad325c52430b34d2e4418
Isaac Hayab <isaachayb0@gmail.com>
2025-11-28 16:46:20 +0000

    Command parsing implementation

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
+#include <unistd.h>
 #include <iostream>
+#include <sched.h>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+vector<char *> parse(string line) {
+  stringstream stream(line);
+  vector<char *> tokens;
+  string t;
+  while (stream >> t)
+    tokens.push_back(const_cast<char *>(t.c_str()));
+  tokens.push_back(nullptr); 
+  return tokens;
+}
 
 int main() {
 
   for (;;) {
+    // Get input
     cout << "$ ";
     string input;
     getline(cin, input);
 
-    if (input == "exit") break;
+    // Parse input
+    vector<char*> tokens = parse(input); 
+
+    // Execute
+    
+    // exit 
+    if (input == "exit")
+      break;
   }
   return 0;
 }
