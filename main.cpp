21f2d16a9eaf117c707b3940d9dd2aaa07be785d
Isaac Hayab <isaachayb0@gmail.com>
2025-11-28 17:46:05 +0000

    cd and ls implementation

--- main.cpp
+++ main.cpp
@@ -28,8 +28,41 @@ int main() {
     string input;
     getline(cin, input);
 
+    // exit
+    if (input == "exit")
+      break;
+
     // Parse input
     vector<char *> args = parse(input);
+
+    // cd
+    if (string cd = "cd"; args[0] == cd) {
+      if (args.size() > 1) {
+        if (chdir(args[1]) != 0) perror("cd failed"); 
+      } else {
+        cerr << "cd: missing arguements" << endl; 
+      }
+      continue;
+    }
+
+    // // author
+    // if (args[0] == "author") {
+    //   cout << "Isaac N. Hayab <isaachayab0@gmail.com>" << endl; 
+    //   continue;
+    // }
+
+    // // gemini
+    // if (args[0] == "gemini") {
+    //   cout << "Hello from Gemini!" << endl;
+    //   continue;
+    // }
+
+    if (args[0] == "cd") {
+      const char *dir = (args.size() > 1) ? args[1] : getenv("HOME");
+      if (!dir) dir = "/";
+      if (chdir(dir) != 0) perror("cd failed"); 
+      continue;
+    }
 
     // Execute
     pid_t pid = fork();
@@ -45,10 +78,6 @@ int main() {
       // Parent process
       wait(nullptr);
     }
-
-    // exit
-    if (input == "exit")
-      break;
   }
   return 0;
 }
